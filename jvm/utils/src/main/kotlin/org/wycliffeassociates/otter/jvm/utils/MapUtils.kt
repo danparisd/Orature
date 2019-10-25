c0f441d3b5d65b550db704308aa29a24630c7827
Russell Aunger <rba@live.com>
2019-10-25 12:16:38 -0400

    Remove needless JVM reflection.

--- jvm/utils/src/main/kotlin/org/wycliffeassociates/otter/jvm/utils/MapUtils.kt
+++ jvm/utils/src/main/kotlin/org/wycliffeassociates/otter/jvm/utils/MapUtils.kt
@@ -6,4 +6,4 @@ import java.util.EnumMap
  * Returns the value associated with the key, or throws an IllegalStateException if the returned value is null
  */
 fun <K : Enum<K>, V> EnumMap<K, V>.getNotNull(key: K?): V = this[key]
-    ?: throw IllegalStateException("Key \"$key\" returned null value in ${this::class.simpleName}")
+    ?: throw IllegalStateException("Key \"$key\" returned null value in EnumMap.getNotNull.")
